56a39ba8cb3fdd2f2262842329ed961066b035bd
Rizanoor <rizanoorfauzan123@gmail.com>
2023-07-10 14:48:58 +0700

    feat: create state pinia

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  ini Footer
+  <br>
+  <button @click="increment">Tambah</button>
+  <button @click="decrement">Kurang</button>
+  <button @click="reset">reset</button>
+</template>
+
+<script setup>
+import { useCounterStore } from '../stores/counter';
+
+const counter = useCounterStore()
+const increment = () => {
+  counter.$patch((state) => {
+    state.count++
+  })
+}
+const decrement = () => {
+  counter.$patch((state) => {
+    state.count--
+  })
+}
+const reset = () => {
+  counter.$reset()
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  ini Header
+  <pre>{{ counter }}</pre>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { useCounterStore } from '../stores/counter'
+
+const counterStore = useCounterStore();
+const counter = computed(() => counterStore.count)
+
+counterStore.$subscribe((mutation, state) => {
+  console.log(mutation, state);
+})
+</script>
